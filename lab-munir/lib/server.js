e50ce1abbde5951f83fb0e071ebb019a15544429
Munir Ibrahim <mibrah04@gmail.com>
2017-06-22 15:23:06 -0700

    finished lab

--- lab-munir/lib/server.js
+++ lab-munir/lib/server.js
@@ -6,33 +6,33 @@ const uuid = require('uuid');
 const router = require('./router.js');
 const model = require('./model.js');
 
-// router.get('/api/notes', (req, res) => {
-//   if (!req.url.query.id) {
-//     res.writeHead(400);
-//     res.end();
-//     return;
-//   }
-//
-//   if (!model[req.url.query.id]) {
-//     res.writeHead(404);
-//     res.end();
-//     return;
-//   }
-//
-//   res.writeHead(200, {
-//     'Content-Type': 'application/json',
-//   });
-//
-//   res.write(JSON.stringify(model[req.url.query.id]));
-//   res.end();
-// });
+//// Register routes with router
+// GET REQUEST
+router.get('/api/notes', (req, res) => {
+  if (!req.url.query.id) {
+    res.writeHead(400);
+    res.end();
+    return;
+  }
 
-// register routes with router
+  if (!model[req.url.query.id]) {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+
+  res.write(JSON.stringify(model[req.url.query.id]));
+  res.end();
+});
+
+// POST REQUEST
 router.post('/api/notes', (req, res) => {
-  console.log('hit /api/notes');
-
   // logic for POST
-  if (!req.body.content) {
+  if (!req.body.name) {
     res.writeHead(400);
     res.end();
     return;
@@ -40,17 +40,81 @@ router.post('/api/notes', (req, res) => {
   // uuid will generate a randome sting that will not conflict with a future random string
   let note = {
     id: uuid.v1(),
-    content: req.body.content,
+    name: req.body.name,
+    date: new Date().toDateString(),
   };
   model[note.id] = note;
-  console.log('STORAGE: ', model);
+
   res.writeHead(200, {
     'Content-Type': 'application/json',
   });
   res.write(JSON.stringify(note));
-  res.end;
+  res.end();
+  return;
+});
+
+// DELETE REQUEST
+router.delete('/api/notes', (req, res) => {
+  if (!req.url.query.id) {
+    res.writeHead(400);
+    res.end();
+    return;
+  }
+  if (!model[req.url.query.id]) {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
+  // delete from memory storage
+  delete model[req.url.query.id];
+
+  res.writeHead(204);
+  res.end();
+  return;
+});
+
+// PUT REQUEST
+router.put('/api/notes', (req, res) => {
+  if (!req.url.query.id) {
+    res.writeHead(400);
+    res.end();
+    return;
+  }
+  if (!model[req.url.query.id]) {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
+
+  let note = {
+    id: req.url.query.id,
+    name: req.body.name,
+    date: new Date().toDateString(),
+  };
+
+  model[req.url.query.id] = note;
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+
+  res.write(JSON.stringify(note));
+  res.end();
+  return;
+});
+
+//
+router.get('/api/simple-resource-name', (req, res) => {
+  if (Object.keys(model).length === 0) {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
+
+  res.writeHead(200);
+  res.write(Object.keys(model).toString());
+  res.end();
   return;
 });
 
 module.exports = http.createServer(router.route);
-// router.route(req, res);
